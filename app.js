780a10ed760b86f6ce6622e73a4365c651c91140
Matiasstewart <matiasstewartusher@hotmail.com>
2021-09-06 19:49:23 -0300

    subiendo archivos del proyecto mercadoLiebreÂ

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const publicpath = path.resolve (__dirname, './public');
+
+app.use (express.static(publicpath));
+
+app.listen(3070, () => {
+    console.log('Servidor corriendo en http://localhost:3070/ !');
+});
+
+app.get('/', (req, res) => {
+    res.sendFile (path.resolve (__dirname , 'views/home.html'))
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile (path.resolve (__dirname , 'views/register.html'))
+});
